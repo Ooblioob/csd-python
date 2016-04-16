0332fb6f7fdf2a64fad730945b3903560c22a2d6
Daniel Davis <daniel.davis@excella.com>
2016-04-16 17:49:54 -0400

    Add test for inserting coins then releasing change

--- test/test_vending_machine.py
+++ test/test_vending_machine.py
@@ -12,5 +12,15 @@ class TestVendingMachine:
         # Assert
         assert_equals(0, result)
 
+    def test_release_change_with_payment_expects_change_returned(self):
+        # Arrange
+        vending_machine = VendingMachine()
+        vending_machine.insert_coin(1)
+
+        # Act
+        result = vending_machine.release_change()
+
+        # Assert
+        assert_greater(result, 0)
 
 
