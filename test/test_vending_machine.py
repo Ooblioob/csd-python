fd7ccad4b07691a1de729b2ff6a04d05ce244d0c
Daniel Davis <daniel.davis@excella.com>
2016-04-16 17:38:32 -0400

    Initial commit; first test!

---
+++ test/test_vending_machine.py
@@ -0,0 +1,16 @@
+from nose.tools import *
+from vending_machine import VendingMachine
+
+class TestVendingMachine:
+    def test_release_change_when_no_payment_expects_0_change(self):
+        # Arrange
+        vending_machine = VendingMachine()
+
+        # Act
+        result = vending_machine.release_change()
+
+        # Assert
+        assert_equals(0, result)
+
+
+
