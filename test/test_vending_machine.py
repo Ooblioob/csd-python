980f3f42adab567f4abf74282f7d5dc381d6158c
Daniel Davis <daniel.davis@excella.com>
2016-07-31 18:11:05 -0400

    Refactored tests to use setup fixture

--- test/test_vending_machine.py
+++ test/test_vending_machine.py
@@ -4,23 +4,24 @@ from nose.tools import *
 from vending_machine import VendingMachine
 
 class TestVendingMachine:
+    def setup(self):
+        self.vending_machine = VendingMachine()
+
     def test_release_change_when_no_payment_expects_0_change(self):
         # Arrange
-        vending_machine = VendingMachine()
 
         # Act
-        result = vending_machine.release_change()
+        result = self.vending_machine.release_change()
 
         # Assert
         assert_that(result, is_(equal_to(0)))
 
     def test_release_change_with_payment_expects_change_returned(self):
         # Arrange
-        vending_machine = VendingMachine()
-        vending_machine.insert_coin(1)
+        self.vending_machine.insert_coin(1)
 
         # Act
-        result = vending_machine.release_change()
+        result = self.vending_machine.release_change()
 
         # Assert
         assert_that(result, is_(greater_than(0)))
@@ -28,21 +29,19 @@ class TestVendingMachine:
     @unittest.skip("buy_product now returns an exception")
     def test_buy_product_with_no_payment_expects_nothing(self):
         # Arrange
-        vending_machine = VendingMachine()
 
         # Act
-        result = vending_machine.buy_product()
+        result = self.vending_machine.buy_product()
 
         # Assert
         assert_that(result, is_(none()))
 
     def test_buy_product_with_payment_expects_product(self):
         # Arrange
-        vending_machine = VendingMachine()
-        vending_machine.insert_coin(1)
+        self.vending_machine.insert_coin(1)
 
         # Act
-        result = vending_machine.buy_product()
+        result = self.vending_machine.buy_product()
 
         # Assert
         assert_that(result, is_(not_none()))
@@ -50,33 +49,30 @@ class TestVendingMachine:
     @raises(RuntimeError)
     def test_buy_product_with_no_payment_expects_exception(self):
         # Arrange
-        vending_machine = VendingMachine()
 
         # Act
-        result = vending_machine.buy_product()
+        result = self.vending_machine.buy_product()
 
         # Assert
         # an exception should be raised
 
     def test_get_message_returns_success_message_with_successful_purchase(self):
         # Arrange
-        vending_machine = VendingMachine()
-        vending_machine.insert_coin(1)
+        self.vending_machine.insert_coin(1)
 
         # Act
-        vending_machine.buy_product()
+        self.vending_machine.buy_product()
 
         # Assert
-        assert_that(vending_machine.message, is_(equal_to("Enjoy!")))
+        assert_that(self.vending_machine.message, is_(equal_to("Enjoy!")))
 
     def test_get_message_returns_insert_money_message_when_purchase_fails(self):
         # Arrange
-        vending_machine = VendingMachine()
 
         # Act
         try:
-            vending_machine.buy_product()
+            self.vending_machine.buy_product()
             assert False, "buy_product should have failed with exception"
         except:
             # Assert
-            assert_that(vending_machine.message, is_(equal_to("Please insert money")))
+            assert_that(self.vending_machine.message, is_(equal_to("Please insert money")))
