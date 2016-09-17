2751639f189247a3b6af22e3a0b2b35316d0582c
Mike Brown <brown.3.mike@gmail.com>
2016-09-17 14:30:21 -0400

    Remove unnecessary code

--- vending_machine.py
+++ vending_machine.py
@@ -1,23 +1,9 @@
 class VendingMachine:
     def __init__(self):
         self.payment = 0
-        self.message = 'Please insert money'
 
     def release_change(self):
         if self.payment > 0:
             return 1
         else:
             return 0
-
-    def insert_coin(self, count):
-        self.payment = count * 25
-
-    def buy_product(self):
-        if self.payment != 0:
-            self.message = 'Enjoy!'
-            return 'product'
-        else:
-            raise RuntimeError("Cannot buy product without payment")
-
-    def get_message(self):
-        return self.message
