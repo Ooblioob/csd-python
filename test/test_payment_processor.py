81941d196041f6fd0f8883bec23d7fdd35543c3c
Mike Brown <mike.brown@excella.com>
2018-08-11 19:27:44 -0400

    update to pytest

--- test/test_payment_processor.py
+++ test/test_payment_processor.py
@@ -1,35 +1,34 @@
-import unittest
-from nose.tools import *
+import pytest
+from hamcrest import *
 from payments import PaymentProcessor
 
-class TestPaymentProcessor:
-    def setup(self):
-        self.processor = PaymentProcessor()
+@pytest.fixture
+def processor():
+    return PaymentProcessor()
 
-    def test_is_payment_made_with_no_payment(self):
-        # Arrange
+def test_is_payment_made_with_no_payment(processor):
+    # Arrange
 
-        # Act
-        result = self.processor.is_payment_made()
+    # Act
+    result = processor.is_payment_made()
 
-        # Assert
-        assert_false(result, "is_payment_made should have returned false for no payment")
+    assert result == False, "is_payment_made should have returned false for no payment"
 
-    def test_is_payment_made_with_a_payment(self):
-        # Arrange
-        self.processor.make_payment(1)
+def test_is_payment_made_with_a_payment(processor):
+    # Arrange
+    processor.make_payment(1)
 
-        # Act
-        result = self.processor.is_payment_made()
+    # Act
+    result = processor.is_payment_made()
 
-        # Assert
-        assert_true(result, "should have returned true because a payment was made")
+    # Assert
+    assert result == True, "should have returned true because a payment was made"
 
-    def test_make_payment_expects_payment_nonzero(self):
-        # Arrange
+def test_make_payment_expects_payment_nonzero(processor):
+    # Arrange
 
-        # Act
-        self.processor.make_payment(1)
+    # Act
+    processor.make_payment(1)
 
-        # Assert
-        assert_not_equal(0, self.processor.payments)
+    # Assert
+    assert processor.payments != 0
--- test/test_vending_machine.py
+++ test/test_vending_machine.py
@@ -1,127 +1,126 @@
-import unittest
+import pytest
 from hamcrest import *
-from nose.tools import *
 from vending_machine import VendingMachine
 try:
     from mock import patch
 except ImportError:
     from unittest.mock import patch
 
-class TestVendingMachine:
-    def setup(self):
-        self.vending_machine = VendingMachine()
+@pytest.fixture
+def vending_machine():
+    return VendingMachine()
 
-    def test_release_change_when_no_payment_expects_0_change(self):
-        # Arrange
+def test_release_change_when_no_payment_expects_0_change(vending_machine):
+    # Arrange
 
-        # Act
-        result = self.vending_machine.release_change()
+    # Act
+    result = vending_machine.release_change()
 
+    # Assert
+    assert result == 0
+    assert_that(result, is_(equal_to(0)))
+
+def test_release_change_with_payment_expects_change_returned(vending_machine):
+    # Arrange
+    vending_machine.insert_coin(1)
+
+    # Act
+    result = vending_machine.release_change()
+
+    # Assert
+    assert result > 0
+    assert_that(result, is_(greater_than(0)))
+
+@pytest.mark.skip("buy_product now returns an exception")
+def test_buy_product_with_no_payment_expects_nothing(vending_machine):
+    # Arrange
+
+    # Act
+    result = vending_machine.buy_product()
+
+    # Assert
+    assert result is None
+    assert_that(result, is_(none()))
+
+@patch("payments.PaymentProcessor.is_payment_made")
+@patch("notifications.HQ.notify")
+def test_buy_product_with_payment_expects_product(notify_mock, is_payment_made_mock, vending_machine):
+    # Arrange
+    is_payment_made_mock.return_value = True
+
+    # Act
+    result = vending_machine.buy_product()
+
+    # Assert
+    assert result is not None
+    assert_that(result, is_(not_none()))
+
+@patch("payments.PaymentProcessor.is_payment_made")
+@patch("notifications.HQ.notify")
+def test_buy_product_with_no_payment_expects_exception(notify_mock, is_payment_made_mock, vending_machine):
+    # Arrange
+    is_payment_made_mock.return_value = False
+
+    # Act
+    #result = vending_machine.buy_product()
+    with pytest.raises(RuntimeError):
+        vending_machine.buy_product()
+    assert_that(vending_machine.buy_product, raises(RuntimeError))
+
+    # Assert
+    # an exception should be raised
+
+@patch("payments.PaymentProcessor.is_payment_made")
+def test_get_message_returns_success_message_with_successful_purchase(is_payment_made_mock, vending_machine):
+    # Arrange
+    is_payment_made_mock.return_value = True
+    vending_machine.insert_coin(1)
+
+    # Act
+    vending_machine.buy_product()
+
+    # Assert
+    assert vending_machine.message == "Enjoy!"
+    assert_that(vending_machine.message, is_(equal_to("Enjoy!")))
+
+@patch("payments.PaymentProcessor.is_payment_made")
+@patch("notifications.HQ.notify")
+def test_get_message_returns_insert_money_message_when_purchase_fails(notify_mock, is_payment_made_mock, vending_machine):
+    # Arrange
+    is_payment_made_mock.return_value = False
+
+    # Act
+    try:
+        vending_machine.buy_product()
+        assert False, "buy_product should have failed with exception"
+    except:
         # Assert
-        assert_true(result == 0)
-        assert_equals(0, result)
-        assert_that(result, is_(equal_to(0)))
+        assert vending_machine.message == "Please insert money"
+        assert_that(vending_machine.message, is_(equal_to("Please insert money")))
 
-    def test_release_change_with_payment_expects_change_returned(self):
-        # Arrange
-        self.vending_machine.insert_coin(1)
 
-        # Act
-        result = self.vending_machine.release_change()
+@patch("payments.PaymentProcessor.is_payment_made")
+@patch("notifications.HQ.notify")
+def test_buy_product_with_payment_sends_notification(notify_mock, is_payment_made_mock, vending_machine):
+    # Arrange
+    is_payment_made_mock.return_value = True
 
+    # Act
+    vending_machine.buy_product()
+
+    # Assert
+    notify_mock.assert_called_with('product purchased')
+
+@patch("payments.PaymentProcessor.is_payment_made")
+@patch("notifications.HQ.notify")
+def test_buy_product_without_payment_sends_notification(notify_mock, is_payment_made_mock, vending_machine):
+    # Arrange
+    is_payment_made_mock.return_value = False
+
+    # Act
+    try:
+        vending_machine.buy_product()
+        assert False, "buy_product should have failed with exception"
+    except:
         # Assert
-        assert_true(result > 0)
-        assert_greater(result, 0)
-        assert_that(result, is_(greater_than(0)))
-
-    @unittest.skip("buy_product now returns an exception")
-    def test_buy_product_with_no_payment_expects_nothing(self):
-        # Arrange
-
-        # Act
-        result = self.vending_machine.buy_product()
-
-        # Assert
-        assert_is_none(result)
-        assert_that(result, is_(none()))
-
-    @patch("payments.PaymentProcessor.is_payment_made")
-    @patch("notifications.HQ.notify")
-    def test_buy_product_with_payment_expects_product(self, notify_mock, is_payment_made_mock):
-        # Arrange
-        is_payment_made_mock.return_value = True
-
-        # Act
-        result = self.vending_machine.buy_product()
-
-        # Assert
-        assert_is_not_none(result)
-        assert_that(result, is_(not_none()))
-
-    #@raises(RuntimeError)
-    @patch("payments.PaymentProcessor.is_payment_made")
-    @patch("notifications.HQ.notify")
-    def test_buy_product_with_no_payment_expects_exception(self, notify_mock, is_payment_made_mock):
-        # Arrange
-        is_payment_made_mock.return_value = False
-
-        # Act
-        #result = self.vending_machine.buy_product()
-
-        # Assert
-        assert_raises(RuntimeError, self.vending_machine.buy_product)
-        assert_that(self.vending_machine.buy_product, raises(RuntimeError))
-
-    @patch("payments.PaymentProcessor.is_payment_made")
-    @patch("notifications.HQ.notify")
-    def test_get_message_returns_success_message_with_successful_purchase(self, notify_mock, is_payment_made_mock):
-        # Arrange
-        is_payment_made_mock.return_value = True
-
-        # Act
-        self.vending_machine.buy_product()
-
-        # Assert
-        assert_equals(self.vending_machine.message, "Enjoy!")
-        assert_that(self.vending_machine.message, is_(equal_to("Enjoy!")))
-
-    @patch("payments.PaymentProcessor.is_payment_made")
-    @patch("notifications.HQ.notify")
-    def test_get_message_returns_insert_money_message_when_purchase_fails(self, notify_mock, is_payment_made_mock):
-        # Arrange
-        is_payment_made_mock.return_value = False
-
-        # Act
-        try:
-            self.vending_machine.buy_product()
-            assert False, "buy_product should have failed with exception"
-        except:
-            # Assert
-            assert_equals(self.vending_machine.message, "Please insert money")
-            assert_that(self.vending_machine.message, is_(equal_to("Please insert money")))
-
-    @patch("payments.PaymentProcessor.is_payment_made")
-    @patch("notifications.HQ.notify")
-    def test_buy_product_with_payment_sends_notification(self, notify_mock, is_payment_made_mock):
-        # Arrange
-        is_payment_made_mock.return_value = True
-
-        # Act
-        self.vending_machine.buy_product()
-
-        # Assert
-        notify_mock.assert_called_with('product purchased')
-
-    @patch("payments.PaymentProcessor.is_payment_made")
-    @patch("notifications.HQ.notify")
-    def test_buy_product_without_payment_sends_notification(self, notify_mock, is_payment_made_mock):
-        # Arrange
-        is_payment_made_mock.return_value = False
-
-        # Act
-        try:
-            self.vending_machine.buy_product()
-            assert False, "buy_product should have failed with exception"
-        except:
-            # Assert
-            notify_mock.assert_called_with('unsuccessful purchase')
+        notify_mock.assert_called_with('unsuccessful purchase')
