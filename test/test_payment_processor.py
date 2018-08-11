921acc1132058886a24a559b0d76a6d80e920b0e
Mike Brown <mike.brown@excella.com>
2018-08-11 19:27:44 -0400

    update to pytest

--- test/test_payment_processor.py
+++ test/test_payment_processor.py
@@ -1,39 +1,38 @@
-import unittest
+import pytest
 from hamcrest import *
-from nose.tools import *
 from payments import PaymentProcessor
 
-class TestPaymentProcessor:
-    def setup(self):
-        self.processor = PaymentProcessor()
+@pytest.fixture
+def processor():
+    return PaymentProcessor()
 
-    def test_is_payment_made_with_no_payment(self):
-        # Arrange
+def test_is_payment_made_with_no_payment(processor):
+    # Arrange
 
-        # Act
-        result = self.processor.is_payment_made()
+    # Act
+    result = processor.is_payment_made()
 
-        # Assert
-        assert_false(result)
-        assert_that(result, is_(equal_to(False)))
+    # Assert
+    assert result == False
+    assert_that(result, is_(equal_to(False)))
 
-    def test_is_payment_made_with_a_payment(self):
-        # Arrange
-        self.processor.make_payment(1)
+def test_is_payment_made_with_a_payment(processor):
+    # Arrange
+    processor.make_payment(1)
 
-        # Act
-        result = self.processor.is_payment_made()
+    # Act
+    result = processor.is_payment_made()
 
-        # Assert
-        assert_true(result)
-        assert_that(result, is_(equal_to(True)))
+    # Assert
+    assert result == True
+    assert_that(result, is_(equal_to(True)))
 
-    def test_make_payment_expects_payment_nonzero(self):
-        # Arrange
+def test_make_payment_expects_payment_nonzero(processor):
+    # Arrange
 
-        # Act
-        self.processor.make_payment(1)
+    # Act
+    processor.make_payment(1)
 
-        # Assert
-        assert_not_equal(0, self.processor.payment)
-        assert_that(self.processor.payment, is_not(equal_to(0)))
+    # Assert
+    assert processor.payment != 0
+    assert_that(processor.payment, is_not(equal_to(0)))
